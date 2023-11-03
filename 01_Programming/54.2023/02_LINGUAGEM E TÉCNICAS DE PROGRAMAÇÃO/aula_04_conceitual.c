478de74235151aed2f5da8e0d54b6e40e6ed12a5
wendherSantos <wendher.santos@outlook.com>
2023-11-03 07:54:15 -0300

    ESOFT

---
+++ 01_Programming/54.2023/02_LINGUAGEM E TÉCNICAS DE PROGRAMAÇÃO/aula_04_conceitual.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int idade;
+    printf("Digite a sua idade: \n");
+    scanf("%d", &idade);
+    printf("A idade digitada foi = %d", idade);
+    return 0; 
+}
---
+++ 01_Programming/54.2023/02_LINGUAGEM E TÉCNICAS DE PROGRAMAÇÃO/main.c
@@ -0,0 +1,5 @@
+int main(int argc, char const *argv[])
+{
+    printf("Olá mundo!");
+    return 0;
+}
